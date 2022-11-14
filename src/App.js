96628f68fdf3f9bffd2e5ec69c5b8f30317aad59
daramosn <daramosn@unal.edu.co>
2022-11-13 19:31:16 -0500

    Nav 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import './App.css';
 
+import Navbar from './components/Navbar/Navbar';
+
 function App() {
   return (
     <Navbar />
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import logo from '../../images/arcabook-1-crop2.png';
+
+import classes from './Navbar.module.css';
 
 const Navbar = (props) => {
-    return ();
+
+    return (
+        <div className={classes.nav}>
+
+            <ul>
+                <li><img src={logo} alt='logo' /></li>
+                <li>Home</li>
+                <li>Contact</li>
+                <li>About</li>
+            </ul>
+
+            <ul>
+                <li>Log in</li>
+                <li>Register</li>
+            </ul>
+
+        </div>
+    );
 };
 
 export default Navbar;
